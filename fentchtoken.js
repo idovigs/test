81cb92bfe691f0c45cdb1e5f9d98e25d000fc7cb
idovigs <dovighullam@gmail.com>
2024-02-05 18:27:43 +0700

    hey

---
+++ fentchtoken.js
@@ -0,0 +1,23 @@
+const { ethers } = require('ethers');
+const contractAddress = '0x6aF48997671584672e084Bf2296473677598ee58';
+const abi = [
+
+];
+
+const userAddress = '0x430eDc777cB934942A69F8964B77c41C97E3bc5C';
+
+const provider = new ethers.providers.JsonRpcProvider('https://evm-rpc.planq.network/');
+
+const tokenContract = new ethers.Contract(contractAddress, abi, provider);
+
+async function fetchTokenBalance() {
+  try {
+  
+    const balance = await tokenContract.balanceOf(userAddress);
+
+    console.log(`Token Balance for ${userAddress}: ${balance.toString()}`);
+  } catch (error) {
+    console.error('Error fetching token balance:', error.message);
+  }
+}
+fetchTokenBalance();
